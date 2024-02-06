315f91fe064ae345fbe87224ac8b6bf72e0ff9db
Abhishek Santra <abhisheksantra28@gmail.com>
2024-02-07 02:19:14 +0530

    Complete navbar and landingpage component

---
+++ ochi/src/components/LandingPage.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FaArrowUpLong } from "react-icons/fa6";
+
+const LandingPage = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-900 pt-1">
+      <div className="textstructure  mt-52 px-20 ">
+        {["We create", "Eye opening", "Presentations"].map((item, index) => (
+          <div className="masker">
+            <div className="w-fit flex items-center overflow-hidden">
+              {index === 1 && (
+                <div className="mr-[1vw] w-[8vw] h-[4vw] rounded bg-red-400"></div>
+              )}
+              <h1 className="uppercase leading-[5vw] tracking-tighter font-semibold font-['FoundersGrotesk'] text-[5.6vw]">
+                {item}
+              </h1>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="border-t-[1px] border-zinc-800 mt-32 flex items-center justify-between py-5 px-20  font-['FoundersGrotesk']">
+        {[
+          "For public and private companies",
+          "From the first pitch to IPO",
+        ].map((item, index) => (
+          <p className="text-md font-light tracking-tight leading-none">
+            {item}
+          </p>
+        ))}
+
+        <div className="start flex items-center gap-5">
+          <div className="px-5 py-2 font-light text-md uppercase rounded-full border-[2px] border-zinc-600">
+            Start the project
+          </div>
+          <div className="w-10 h-10 flex items-center justify-center border-[2px] border-zinc-600 rounded-full">
+            <span className="rotate-[45deg]">
+              <FaArrowUpLong />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
